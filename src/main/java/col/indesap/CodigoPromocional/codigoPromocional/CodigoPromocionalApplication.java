6092b6e86a6e152fae6e06b87a0ada731c20d4ee
Luanchev <Luanchev3@gmail.com>
2024-08-30 09:08:45 -0500

    v1-inicio

---
+++ src/main/java/col/indesap/CodigoPromocional/codigoPromocional/CodigoPromocionalApplication.java
@@ -0,0 +1,13 @@
+package col.indesap.CodigoPromocional.codigoPromocional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoPromocionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoPromocionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/col/indesap/CodigoPromocional/codigoPromocional/CodigoPromocionalApplicationTests.java
@@ -0,0 +1,13 @@
+package col.indesap.CodigoPromocional.codigoPromocional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoPromocionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
